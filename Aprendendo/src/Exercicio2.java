f2258a541568c2fc232c4ff87e916e59c2cb6325
lucasfreire089 <lucas.freire@dcomp.ufs.br>
2023-12-02 12:40:51 -0300

    aula kipperdev

---
+++ Aprendendo/src/Exercicio2.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Exercicio2 {
+     public static void main(String[]args){
+//       double idade = 22.50;
+//       if (idade>22.60){
+//         System.out.println("Maior");
+//       } else if (idade == 22.60){
+//         System.out.println("Igual");
+
+//       }
+//       else System.out.println("Menor");
+    int[] idades = {2,3,5,6,7,8};
+    idades[0] = 4;
+System.out.println(Arrays.toString(idades));
+
+}
+ }
+
