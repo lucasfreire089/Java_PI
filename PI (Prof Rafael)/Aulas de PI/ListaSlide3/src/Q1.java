2bca0adf8034aa38c8afe3b67036621d24e95e6f
lucasfreire089 <lucas.freire@dcomp.ufs.br>
2024-01-04 21:33:44 -0300

    wd

---
+++ PI (Prof Rafael)/Aulas de PI/ListaSlide3/src/Q1.java
@@ -0,0 +1,19 @@
+// Elaborar um algoritmo que imprima números entre 0
+// e 20, em ordem ascendente.
+
+
+public class Q1 {
+    public static void main(String[] args) throws Exception {
+
+
+int numero = 0
+
+while (numero<=20){
+System.out.println(numero);
+}
+
+numero++;
+
+
+    }
+}
